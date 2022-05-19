f1659157286389ce14ede7f4e32cba14fc0393a4
Spencer Skett <sskett@gmail.com>
2022-05-19 22:39:11 +0800

    Sorted issue with conversion of strings back to vectors (nan values present)
    Tuned clustering to provide usable results (~20-40 clusters).

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     define_pandas_options()
     options = {
         # no errors in 'weeks_to_import': [1, 2, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 16, 17],
-        'weeks_to_import': [1],
+        'weeks_to_import': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
         'players_to_import_by_id': [],
         'players_to_import_by_name': [],
         'games_to_import': [],
