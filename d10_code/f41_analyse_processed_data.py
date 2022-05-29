e5a2809f980a9996dfd1ec496bc51fec180326f7
sskett@gmail.com <_D67hcN42>
2022-05-29 15:06:05 +0800

    Implemented SVM

--- d10_code/f41_analyse_processed_data.py
+++ d10_code/f41_analyse_processed_data.py
@@ -2,10 +2,33 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from sklearn import datasets, svm, metrics
+from sklearn.model_selection import train_test_split
+
 
 def analyse_routes_data(routes_df):
     x, y = get_limits(routes_df)
-    plot_route_data(routes_df)
+    plot_route_data(routes_df, x, y)
+    plot_route_probs(routes_df)
+
+    dims = (int(abs(x[0]) + abs(x[1])), int(abs(y[0]) + abs(y[1])))
+    routes_df['grids'] = get_position_grid(routes_df, dims)
+
+    x_train, x_test, y_train, y_test = train_test_split(routes_df['grids'], routes_df['route'], test_size=0.2, shuffle=False,
+                                                        random_state=1)
+
+    do_svm_analysis(x_train.tolist(), y_train, x_test.tolist(), y_test)
+    do_MLP_analysis(routes_df)
+
+
+def get_position_grid(df, dims):
+    grids = []
+    for idx, row in df.iterrows():
+        grid = np.zeros(dims[0] * dims[1]).reshape(dims[0], dims[1])
+        for position in row['pos']:
+            grid[int(position[0]), int(position[1])] = position[2]
+        grids.append(grid.reshape(dims[0] * dims[1]))
+    return grids
 
 
 def get_limits(df):
@@ -31,3 +54,35 @@ def plot_route_data(df, x_lims, y_lims):
     plt.xlim(x_lims[0] - 1, x_lims[1] + 1)
     plt.ylim(y_lims[0] - 1, y_lims[1] + 1)
     plt.show()
+
+
+def do_svm_analysis(x_train, y_train, x_test, y_test):
+
+    # Create a classifier: a support vector classifier
+    clf = svm.SVC(gamma=0.005, kernel='rbf')
+
+    # Learn on the train subset
+    clf.fit(x_train, y_train)
+
+    # Predict the value of the test subset
+    predicted = clf.predict(x_test)
+
+    print(
+        f'Classification report for classifier {clf}:\n'
+        f'{metrics.classification_report(y_test, predicted)}\n'
+    )
+
+    disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
+    disp.figure_.suptitle('Confusion Matrix')
+    print(f'Confusion Matrix:\n{disp.confusion_matrix}')
+    plt.show()
+
+
+def do_MLP_analysis(df):
+
+
+
+def plot_route_probs(df):
+    plt.figure(figsize=(20, 10))
+    plt.bar(df['route'].value_counts().index,
+            df['route'].value_counts().values / df['route'].value_counts().sum(), width=0.75)
